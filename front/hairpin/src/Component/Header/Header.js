396fa21f7c8e699c9bd84e721fc9c011354164a0
PILSUCHOI <chlvlftn22@gmail.com>
2023-12-08 17:11:12 +0900

    feat/front-Header

---
+++ front/hairpin/src/Component/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="group-3">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group-2">
+          <img
+            className="vector-2"
+            alt="Vector"
+            src="https://c.animaapp.com/4IIPmODq/img/vector.svg"
+          />
+          <div className="text-wrapper-10">지역을 검색하세요</div>
+        </div>
+      </div>
+      <img
+        className="group-4"
+        alt="Group"
+        src="https://c.animaapp.com/4IIPmODq/img/group-48@2x.png"
+      />
+      <div className="logo-hairpin">Hairpin</div>
+    </div>
+  );
+}
