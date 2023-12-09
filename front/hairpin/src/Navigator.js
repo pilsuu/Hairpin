3e72e7af31e9a27d6cd651ce805554f74a9c8249
PILSUCHOI <chlvlftn22@gmail.com>
2023-12-09 23:09:00 +0900

    feat/front-Nav-add

--- front/hairpin/src/Navigator.js
+++ front/hairpin/src/Navigator.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-import { HomePage } from "../src/Pages/HomePage/HomePage";
+import HomePage from "../src/Pages/HomePage/HomePage";
+import MatchReservePage from "./Pages/MatchReservePage/MatchReservePage";
 
 export default function Navigator() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route path="/match-reserve" element={<MatchReservePage />} />
       </Routes>
     </BrowserRouter>
   );
