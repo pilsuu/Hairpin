423f6a9e048868e13d2bdf6f69710759bdc3ea71
PILSUCHOI <chlvlftn22@gmail.com>
2023-12-09 20:59:18 +0900

    feat/front-MatchList

---
+++ front/hairpin/src/Component/MatchList/MatchList.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./MatchList.css";
+
+const matches = [
+  {
+    time: "10:00",
+    place: "강남 해피배드민턴장",
+    gameType: "단식 경기",
+    gender: "남녀모두",
+    buttonText: "신청가능",
+  },
+  {
+    time: "13:00",
+    place: "용산 해피배드민턴장",
+    gameType: "복식 경기",
+    gender: "남자",
+    buttonText: "신청가능",
+  },
+  {
+    time: "21:00",
+    place: "수원 해피배드민턴장",
+    gameType: "복식 경기",
+    gender: "남녀모두",
+    buttonText: "신청가능",
+  },
+  {
+    time: "19:00",
+    place: "서울대입구 해피배드민턴장",
+    gameType: "복식 경기",
+    gender: "남녀모두",
+    buttonText: "신청가능",
+  },
+  {
+    time: "17:00",
+    place: "도봉 해피배드민턴장",
+    gameType: "단식 경기",
+    gender: "여자",
+    buttonText: "신청가능",
+  },
+  {
+    time: "22:00",
+    place: "건대입구 해피배드민턴장",
+    gameType: "복식 경기",
+    gender: "남녀모두",
+    buttonText: "신청가능",
+  },
+  {
+    time: "21:00",
+    place: "과천 해피배드민턴장",
+    gameType: "복식 경기",
+    gender: "남녀모두",
+    buttonText: "신청가능",
+  },
+];
+
+export default function MatchList() {
+  return (
+    <div className="MatchList-view">
+      {matches.map((match) => (
+        <MatchItem key={match.time} {...match} />
+      ))}
+    </div>
+  );
+}
+
+function MatchItem({ time, place, gameType, gender, buttonText }) {
+  return (
+    <div className="match-item">
+      <div className="match-time">{time}</div>
+      <div className="match-place">{place}</div>
+      <div className="match-type">{gameType}</div>
+      <div className="gender-type">{gender}</div>
+      <div className="match-list-button-wrapper">
+        <div
+          className="match-list-button-text-wrapper"
+          onClick={() => alert("신청되었습니다.")}
+        >
+          {buttonText}
+        </div>
+      </div>
+    </div>
+  );
+}
