fb5ecfa4f117d09c67d9db76e1147603454995a9
PILSUCHOI <chlvlftn22@gmail.com>
2023-12-09 21:00:46 +0900

    feat/front-Home-Page-UI

--- front/hairpin/src/Pages/HomePage/HomePage.js
+++ front/hairpin/src/Pages/HomePage/HomePage.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../../Component/Header/Header";
+import MatchDate from "../../Component/MatchDate/MatchDate";
+import Filter from "../../Component/Filter/Filter";
+import MatchList from "../../Component/MatchList/MatchList";
 
 export function HomePage() {
   return (
     <div>
       <Header />
+      <MatchDate />
+      <Filter />
+      <MatchList />
     </div>
   );
 }
