3720d91ee9e2f7dc6f17566a014ed8fd971a766f
PILSUCHOI <chlvlftn22@gmail.com>
2023-12-09 22:45:40 +0900

    feat/front-HomePage-UI-edit

--- front/hairpin/src/Pages/HomePage/HomePage.js
+++ front/hairpin/src/Pages/HomePage/HomePage.js
@@ -3,14 +3,19 @@ import Header from "../../Component/Header/Header";
 import MatchDate from "../../Component/MatchDate/MatchDate";
 import Filter from "../../Component/Filter/Filter";
 import MatchList from "../../Component/MatchList/MatchList";
+import "./HomePage.css";
 
-export function HomePage() {
+export default function HomePage() {
   return (
-    <div>
-      <Header />
-      <MatchDate />
-      <Filter />
-      <MatchList />
+    <div className="HomePage-wrapper">
+      <div className="screen-balancer">
+        <div className="screen-balancer-2">
+          <Header />
+          <MatchDate />
+          <Filter />
+          <MatchList />
+        </div>
+      </div>
     </div>
   );
 }
